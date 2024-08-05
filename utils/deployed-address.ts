8f637b75827447115c0d3800b1720eab5a2c1e27
Ronggo SWS <aveironett@gmail.com>
2024-08-05 15:02:45 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xc5E0Ea73AE050E4F6A62E1Fc2a9952258Fa6d8Cc'
+export const ProxyAdmin = '0x5253cBEA195E12E97121716c4d7C83C318fa86C4'
+export const SWTRImplementation = '0x8d5Db47E87396Dc1938EE2101F09C246c3bd7394'
     
